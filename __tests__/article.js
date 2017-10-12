9d4a3c7d2e29e530f65e81acbb1a65d7d194e8ac
Mike Morley <mike.morley@9eggs.com>
2017-10-12 17:54:05 +0100

    We have a test with Jest

--- __tests__/article.js
+++ __tests__/article.js
@@ -1,6 +1,15 @@
+const mustache = require('mustache');
+var read = require('read-file');
+const html = read.sync('./src/elements/article.html', 'utf8');
 
+test('empty test of article', () => {
+    expect(mustache.render(html, {})).toBe('<article></article>');
+});
 
+test('hidden test of article', () => {
+    expect(mustache.render(html, {articleHidden: true})).toBe('<article aria-hidden="true"></article>');
+});
 
-test('adds 1 + 2 to make 3', () => {
-    expect('a').toBe('a');
+test('content test of article', () => {
+    expect(mustache.render(html, {article: 'some text'})).toBe('<article>some text</article>');
 });
